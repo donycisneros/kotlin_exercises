8212e1b1fe163baf8adf1a097b9469d5c9ae4859
Dony Cisneros <dony.cisne@gmail.com>
2020-12-11 23:37:48 -0300

    Initial repo setup

---
+++ src/01-variables.kt
@@ -0,0 +1,9 @@
+//https://kotlinlang.org/docs/reference/basic-syntax.html#defining-variables
+
+fun main() {
+    val userName: String = "Dony"
+    val useAge: Int = 25
+
+    println("Welcome $userName!")
+    println("You are $useAge years old.")
+}
